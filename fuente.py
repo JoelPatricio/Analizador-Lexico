702bb34346f3db495780052bb0ecf95df4d34fcb
EdgarEE <54454087+EdgarEE@users.noreply.github.com>
2020-10-17 00:01:45 -0500

    Add files via upload

--- fuente.py
+++ fuente.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import QFile, QIODevice, Qt, QRect, QSize
 from PyQt5.QtWidgets import QFileDialog, QTableWidgetItem
 from ventana1 import *
+import numpy as np
 from PyQt5.QtWidgets import (QWidget, QPlainTextEdit, QTextEdit, QMainWindow, QAction, qApp, QApplication)
 from PyQt5.QtGui import QColor, QPainter, QTextFormat, QKeySequence, QFont
 
@@ -9,6 +10,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
+
         header = self.tableWidget.horizontalHeader()  
         header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
         header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
@@ -17,7 +19,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
 
         self.pushButton_2.clicked.connect(self.cargarArchivo)
+        self.pushButton_4.clicked.connect(self.palabrasResCargar)
         self.pushButton.clicked.connect(self.iniciar)
+
+        self.show()
         
         
         
@@ -39,15 +44,26 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.pushButton.setEnabled(True)
         pass
 
+    def palabrasResCargar(self):
+        archivo = QFileDialog.getOpenFileName(self)
+
+        if archivo[0]:
+            with open(archivo[0], 'rt' ) as f:
+                datos= f.read()
+
+                self.pushButton.setEnabled(True)
+
+        pass
+
     def iniciar(self):
-        #palabrasReservadas=[]
+        palabrasReservadas = str
+        for linea in palabrasReservadas.readlines():
+                    palabrasReservadas=[]
+
+                    palabrasReservadas.append(linea)
         
         
         pass
-
-
-    
-
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
