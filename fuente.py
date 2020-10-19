d0e4255767f8febfae73b1411db742c7e9a0fcd2
Jhp32 <marine.dark@gmail.com>
2020-10-19 18:34:42 -0500

    Cambio de lugar botones

--- fuente.py
+++ fuente.py
@@ -1,34 +1,115 @@
-from PyQt5.QtCore import QFile, QIODevice
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtCore import QFile, QIODevice, Qt, QRect, QSize
+from PyQt5.QtWidgets import QFileDialog, QTableWidgetItem
 from ventana1 import *
-import PyQt5.QtCore
+from PyQt5.QtWidgets import (QWidget, QPlainTextEdit, QTextEdit, QMainWindow, QAction, qApp, QApplication)
+from PyQt5.QtGui import QColor, QPainter, QTextFormat, QKeySequence, QFont
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
+        header = self.tableWidget.horizontalHeader()  
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        header = self.tableWidget_2.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
 
         self.pushButton_2.clicked.connect(self.cargarArchivo)
-        self.pushButton.clicked.connect(self.iniciar)
+        self.pushButton_3.clicked.connect(self.leerAutomata)        
+        self.pushButton_4.clicked.connect(self.cargarPalabrasReservadas)
+        self.pushButton.clicked.connect(self.iniciarAnalisis)
+
+        
+        #columna=self.tableWidget.insertRow(self.tableWidget.rowCount())        
+        #item1=QTableWidgetItem("Palabra Reservada")
+        #item2 = QTableWidgetItem("while")
+        #self.tableWidget.setItem(0, 0, item1)
+        #self.tableWidget.setItem(0, 1, item2)
+        
 
     def cargarArchivo(self):
         self.plainTextEdit.clear()
         archivo = QFileDialog.getOpenFileName(self)
         archivo = str(archivo[0])
-        contenido=open(archivo,'r')
-        for linea in contenido.readlines():
-            self.plainTextEdit.appendPlainText(linea)
-        contenido.close()
+        global textoAux
+        contenidoTxt=open(archivo,'r')
+        linea=""
+        texto=''
+        for linea in contenidoTxt.readlines():
+            texto =texto.__add__(linea)
+        contenidoTxt.close()
+        textoAux=''
+        for linea in range(3,len(texto)):
+            textoAux = textoAux.__add__(texto[linea])            
+        self.plainTextEdit.appendPlainText(textoAux)
         self.pushButton.setEnabled(True)
         pass
 
-    def iniciar(self):
-        palabrasReservadas=[]
-        
+    def leerEstadosFinales(self,linea):
+        estadosf=linea
+        estadosf=estadosf.replace('\n','')
+        estadosf=estadosf.split(',')
+        global estadosFinales
+        estadosFinales=[]
+        for x in estadosf:
+            x=x.split(':')
+            x[1]=x[1].replace('\n','')
+            estadosFinales.append(dict.fromkeys(x[0],x[1]))
         pass
 
+    def crearAutomata(self,linea):
+        linea=linea.replace('\n','')
+        linea=linea.split(',')
+        estadoN=[]
+        for x in linea:
+            x=x.split(':')
+            estadoN.append(dict.fromkeys(x[0],x[1]))
+        return estadoN
+        pass
 
+    def leerAutomata(self):
+        archivo = QFileDialog.getOpenFileName(self)
+        archivo = str(archivo[0])
+        contenido = open(archivo, 'r')
+        global tablaTransicion
+        tablaTransicion=[]
+        primeraLinea=True
+        for linea in contenido.readlines():
+            if(primeraLinea==True):
+                self.leerEstadosFinales(linea)
+                primeraLinea=False
+            else:
+                linea=linea.split("=")
+                tablaTransicion.append(dict.fromkeys(linea[0],self.crearAutomata(linea[1])))
+        contenido.close()
+        print(palabrasReservadas)
+        pass
+
+    def cargarPalabrasReservadas(self):
+        archivo = QFileDialog.getOpenFileName(self)
+        archivo = str(archivo[0])        
+        contenido=open(archivo,'r')
+        global palabrasReservadas
+        palabrasReservadas = []
+        for linea in contenido.readlines():
+            linea=linea.rstrip('\n')
+            palabrasReservadas.append(linea)
+        contenido.close()
+        pass
+
+    def iniciarAnalisis(self):
+        tam=len(textoAux)
+        i=0
+        while(i<tam):
+            
+
+
+
+            i=i+1
+
+        pass
     
 
 
--- ventana1.py
+++ ventana1.py
@@ -14,494 +14,40 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(758, 484)
-        MainWindow.setStyleSheet("QToolTip\n"
-"{\n"
-"     border: 1px solid black;\n"
-"     background-color: #ffa02f;\n"
-"     padding: 1px;\n"
-"     border-radius: 3px;\n"
-"     opacity: 100;\n"
-"}\n"
-"\n"
-"QWidget\n"
-"{\n"
-"    color: #b1b1b1;\n"
-"    background-color: #323232;\n"
-"}\n"
-"\n"
-"QWidget:item:hover\n"
-"{\n"
-"    background-color: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 1 #ca0619);\n"
-"    color: #000000;\n"
-"}\n"
-"\n"
-"QWidget:item:selected\n"
-"{\n"
-"    background-color: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 1 #d7801a);\n"
-"}\n"
-"\n"
-"QMenuBar::item\n"
-"{\n"
-"    background: transparent;\n"
-"}\n"
-"\n"
-"QMenuBar::item:selected\n"
-"{\n"
-"    background: transparent;\n"
-"    border: 1px solid #ffaa00;\n"
-"}\n"
-"\n"
-"QMenuBar::item:pressed\n"
-"{\n"
-"    background: #444;\n"
-"    border: 1px solid #000;\n"
-"    background-color: QLinearGradient(\n"
-"        x1:0, y1:0,\n"
-"        x2:0, y2:1,\n"
-"        stop:1 #212121,\n"
-"        stop:0.4 #343434/*,\n"
-"        stop:0.2 #343434,\n"
-"        stop:0.1 #ffaa00*/\n"
-"    );\n"
-"    margin-bottom:-1px;\n"
-"    padding-bottom:1px;\n"
-"}\n"
-"\n"
-"QMenu\n"
-"{\n"
-"    border: 1px solid #000;\n"
-"}\n"
-"\n"
-"QMenu::item\n"
-"{\n"
-"    padding: 2px 20px 2px 20px;\n"
-"}\n"
-"\n"
-"QMenu::item:selected\n"
-"{\n"
-"    color: #000000;\n"
-"}\n"
-"\n"
-"QWidget:disabled\n"
-"{\n"
-"    color: #404040;\n"
-"    background-color: #323232;\n"
-"}\n"
-"\n"
-"QAbstractItemView\n"
-"{\n"
-"    background-color: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #4d4d4d, stop: 0.1 #646464, stop: 1 #5d5d5d);\n"
-"}\n"
-"\n"
-"QWidget:focus\n"
-"{\n"
-"    /*border: 2px solid QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 1 #d7801a);*/\n"
-"}\n"
-"\n"
-"QLineEdit\n"
-"{\n"
-"    background-color: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #4d4d4d, stop: 0 #646464, stop: 1 #5d5d5d);\n"
-"    padding: 1px;\n"
-"    border-style: solid;\n"
-"    border: 1px solid #1e1e1e;\n"
-"    border-radius: 5;\n"
-"}\n"
-"\n"
-"QPushButton\n"
-"{\n"
-"    color: #b1b1b1;\n"
-"    background-color: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #565656, stop: 0.1 #525252, stop: 0.5 #4e4e4e, stop: 0.9 #4a4a4a, stop: 1 #464646);\n"
-"    border-width: 1px;\n"
-"    border-color: #1e1e1e;\n"
-"    border-style: solid;\n"
-"    border-radius: 6;\n"
-"    padding: 3px;\n"
-"    font-size: 12px;\n"
-"    padding-left: 5px;\n"
-"    padding-right: 5px;\n"
-"}\n"
-"\n"
-"QPushButton:pressed\n"
-"{\n"
-"    background-color: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #2d2d2d, stop: 0.1 #2b2b2b, stop: 0.5 #292929, stop: 0.9 #282828, stop: 1 #252525);\n"
-"}\n"
-"\n"
-"QComboBox\n"
-"{\n"
-"    selection-background-color: #ffaa00;\n"
-"    background-color: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #565656, stop: 0.1 #525252, stop: 0.5 #4e4e4e, stop: 0.9 #4a4a4a, stop: 1 #464646);\n"
-"    border-style: solid;\n"
-"    border: 1px solid #1e1e1e;\n"
-"    border-radius: 5;\n"
-"}\n"
-"\n"
-"QComboBox:hover,QPushButton:hover\n"
-"{\n"
-"    border: 2px solid QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 1 #d7801a);\n"
-"}\n"
-"\n"
-"\n"
-"QComboBox:on\n"
-"{\n"
-"    padding-top: 3px;\n"
-"    padding-left: 4px;\n"
-"    background-color: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #2d2d2d, stop: 0.1 #2b2b2b, stop: 0.5 #292929, stop: 0.9 #282828, stop: 1 #252525);\n"
-"    selection-background-color: #ffaa00;\n"
-"}\n"
-"\n"
-"QComboBox QAbstractItemView\n"
-"{\n"
-"    border: 2px solid darkgray;\n"
-"    selection-background-color: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 1 #d7801a);\n"
-"}\n"
-"\n"
-"QComboBox::drop-down\n"
-"{\n"
-"     subcontrol-origin: padding;\n"
-"     subcontrol-position: top right;\n"
-"     width: 15px;\n"
-"\n"
-"     border-left-width: 0px;\n"
-"     border-left-color: darkgray;\n"
-"     border-left-style: solid; /* just a single line */\n"
-"     border-top-right-radius: 3px; /* same radius as the QComboBox */\n"
-"     border-bottom-right-radius: 3px;\n"
-" }\n"
-"\n"
-"QComboBox::down-arrow\n"
-"{\n"
-"     image: url(:/down_arrow.png);\n"
-"}\n"
-"\n"
-"QGroupBox:focus\n"
-"{\n"
-"border: 2px solid QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 1 #d7801a);\n"
-"}\n"
-"\n"
-"QTextEdit:focus\n"
-"{\n"
-"    border: 2px solid QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 1 #d7801a);\n"
-"}\n"
-"\n"
-"QScrollBar:horizontal {\n"
-"     border: 1px solid #222222;\n"
-"     background: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0.0 #121212, stop: 0.2 #282828, stop: 1 #484848);\n"
-"     height: 7px;\n"
-"     margin: 0px 16px 0 16px;\n"
-"}\n"
-"\n"
-"QScrollBar::handle:horizontal\n"
-"{\n"
-"      background: QLinearGradient( x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #ffa02f, stop: 0.5 #d7801a, stop: 1 #ffa02f);\n"
-"      min-height: 20px;\n"
-"      border-radius: 2px;\n"
-"}\n"
-"\n"
-"QScrollBar::add-line:horizontal {\n"
-"      border: 1px solid #1b1b19;\n"
-"      border-radius: 2px;\n"
-"      background: QLinearGradient( x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #ffa02f, stop: 1 #d7801a);\n"
-"      width: 14px;\n"
-"      subcontrol-position: right;\n"
-"      subcontrol-origin: margin;\n"
-"}\n"
-"\n"
-"QScrollBar::sub-line:horizontal {\n"
-"      border: 1px solid #1b1b19;\n"
-"      border-radius: 2px;\n"
-"      background: QLinearGradient( x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #ffa02f, stop: 1 #d7801a);\n"
-"      width: 14px;\n"
-"     subcontrol-position: left;\n"
-"     subcontrol-origin: margin;\n"
-"}\n"
-"\n"
-"QScrollBar::right-arrow:horizontal, QScrollBar::left-arrow:horizontal\n"
-"{\n"
-"      border: 1px solid black;\n"
-"      width: 1px;\n"
-"      height: 1px;\n"
-"      background: white;\n"
-"}\n"
-"\n"
-"QScrollBar::add-page:horizontal, QScrollBar::sub-page:horizontal\n"
-"{\n"
-"      background: none;\n"
-"}\n"
-"\n"
-"QScrollBar:vertical\n"
-"{\n"
-"      background: QLinearGradient( x1: 0, y1: 0, x2: 1, y2: 0, stop: 0.0 #121212, stop: 0.2 #282828, stop: 1 #484848);\n"
-"      width: 7px;\n"
-"      margin: 16px 0 16px 0;\n"
-"      border: 1px solid #222222;\n"
-"}\n"
-"\n"
-"QScrollBar::handle:vertical\n"
-"{\n"
-"      background: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 0.5 #d7801a, stop: 1 #ffa02f);\n"
-"      min-height: 20px;\n"
-"      border-radius: 2px;\n"
-"}\n"
-"\n"
-"QScrollBar::add-line:vertical\n"
-"{\n"
-"      border: 1px solid #1b1b19;\n"
-"      border-radius: 2px;\n"
-"      background: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 1 #d7801a);\n"
-"      height: 14px;\n"
-"      subcontrol-position: bottom;\n"
-"      subcontrol-origin: margin;\n"
-"}\n"
-"\n"
-"QScrollBar::sub-line:vertical\n"
-"{\n"
-"      border: 1px solid #1b1b19;\n"
-"      border-radius: 2px;\n"
-"      background: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #d7801a, stop: 1 #ffa02f);\n"
-"      height: 14px;\n"
-"      subcontrol-position: top;\n"
-"      subcontrol-origin: margin;\n"
-"}\n"
-"\n"
-"QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical\n"
-"{\n"
-"      border: 1px solid black;\n"
-"      width: 1px;\n"
-"      height: 1px;\n"
-"      background: white;\n"
-"}\n"
-"\n"
-"\n"
-"QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical\n"
-"{\n"
-"      background: none;\n"
-"}\n"
-"\n"
-"QTextEdit\n"
-"{\n"
-"    background-color: #242424;\n"
-"}\n"
-"\n"
-"QPlainTextEdit\n"
-"{\n"
-"    background-color: #242424;\n"
-"}\n"
-"\n"
-"QHeaderView::section\n"
-"{\n"
-"    background-color: QLinearGradient(x1:0, y1:0, x2:0, y2:1, stop:0 #616161, stop: 0.5 #505050, stop: 0.6 #434343, stop:1 #656565);\n"
-"    color: white;\n"
-"    padding-left: 4px;\n"
-"    border: 1px solid #6c6c6c;\n"
-"}\n"
-"\n"
-"QCheckBox:disabled\n"
-"{\n"
-"color: #414141;\n"
-"}\n"
-"\n"
-"QDockWidget::title\n"
-"{\n"
-"    text-align: center;\n"
-"    spacing: 3px; /* spacing between items in the tool bar */\n"
-"    background-color: QLinearGradient(x1:0, y1:0, x2:0, y2:1, stop:0 #323232, stop: 0.5 #242424, stop:1 #323232);\n"
-"}\n"
-"\n"
-"QDockWidget::close-button, QDockWidget::float-button\n"
-"{\n"
-"    text-align: center;\n"
-"    spacing: 1px; /* spacing between items in the tool bar */\n"
-"    background-color: QLinearGradient(x1:0, y1:0, x2:0, y2:1, stop:0 #323232, stop: 0.5 #242424, stop:1 #323232);\n"
-"}\n"
-"\n"
-"QDockWidget::close-button:hover, QDockWidget::float-button:hover\n"
-"{\n"
-"    background: #242424;\n"
-"}\n"
-"\n"
-"QDockWidget::close-button:pressed, QDockWidget::float-button:pressed\n"
-"{\n"
-"    padding: 1px -1px -1px 1px;\n"
-"}\n"
-"\n"
-"QMainWindow::separator\n"
-"{\n"
-"    background-color: QLinearGradient(x1:0, y1:0, x2:0, y2:1, stop:0 #161616, stop: 0.5 #151515, stop: 0.6 #212121, stop:1 #343434);\n"
-"    color: white;\n"
-"    padding-left: 4px;\n"
-"    border: 1px solid #4c4c4c;\n"
-"    spacing: 3px; /* spacing between items in the tool bar */\n"
-"}\n"
-"\n"
-"QMainWindow::separator:hover\n"
-"{\n"
-"\n"
-"    background-color: QLinearGradient(x1:0, y1:0, x2:0, y2:1, stop:0 #d7801a, stop:0.5 #b56c17 stop:1 #ffa02f);\n"
-"    color: white;\n"
-"    padding-left: 4px;\n"
-"    border: 1px solid #6c6c6c;\n"
-"    spacing: 3px; /* spacing between items in the tool bar */\n"
-"}\n"
-"\n"
-"QToolBar::handle\n"
-"{\n"
-"     spacing: 3px; /* spacing between items in the tool bar */\n"
-"     background: url(:/images/handle.png);\n"
-"}\n"
-"\n"
-"QMenu::separator\n"
-"{\n"
-"    height: 2px;\n"
-"    background-color: QLinearGradient(x1:0, y1:0, x2:0, y2:1, stop:0 #161616, stop: 0.5 #151515, stop: 0.6 #212121, stop:1 #343434);\n"
-"    color: white;\n"
-"    padding-left: 4px;\n"
-"    margin-left: 10px;\n"
-"    margin-right: 5px;\n"
-"}\n"
-"\n"
-"QProgressBar\n"
-"{\n"
-"    border: 2px solid grey;\n"
-"    border-radius: 5px;\n"
-"    text-align: center;\n"
-"}\n"
-"\n"
-"QProgressBar::chunk\n"
-"{\n"
-"    background-color: #d7801a;\n"
-"    width: 2.15px;\n"
-"    margin: 0.5px;\n"
-"}\n"
-"\n"
-"QTabBar::tab {\n"
-"    color: #b1b1b1;\n"
-"    border: 1px solid #444;\n"
-"    border-bottom-style: none;\n"
-"    background-color: #323232;\n"
-"    padding-left: 10px;\n"
-"    padding-right: 10px;\n"
-"    padding-top: 3px;\n"
-"    padding-bottom: 2px;\n"
-"    margin-right: -1px;\n"
-"}\n"
-"\n"
-"QTabWidget::pane {\n"
-"    border: 1px solid #444;\n"
-"    top: 1px;\n"
-"}\n"
-"\n"
-"QTabBar::tab:last\n"
-"{\n"
-"    margin-right: 0; /* the last selected tab has nothing to overlap with on the right */\n"
-"    border-top-right-radius: 3px;\n"
-"}\n"
-"\n"
-"QTabBar::tab:first:!selected\n"
-"{\n"
-" margin-left: 0px; /* the last selected tab has nothing to overlap with on the right */\n"
-"\n"
-"\n"
-"    border-top-left-radius: 3px;\n"
-"}\n"
-"\n"
-"QTabBar::tab:!selected\n"
-"{\n"
-"    color: #b1b1b1;\n"
-"    border-bottom-style: solid;\n"
-"    margin-top: 3px;\n"
-"    background-color: QLinearGradient(x1:0, y1:0, x2:0, y2:1, stop:1 #212121, stop:.4 #343434);\n"
-"}\n"
-"\n"
-"QTabBar::tab:selected\n"
-"{\n"
-"    border-top-left-radius: 3px;\n"
-"    border-top-right-radius: 3px;\n"
-"    margin-bottom: 0px;\n"
-"}\n"
-"\n"
-"QTabBar::tab:!selected:hover\n"
-"{\n"
-"    /*border-top: 2px solid #ffaa00;\n"
-"    padding-bottom: 3px;*/\n"
-"    border-top-left-radius: 3px;\n"
-"    border-top-right-radius: 3px;\n"
-"    background-color: QLinearGradient(x1:0, y1:0, x2:0, y2:1, stop:1 #212121, stop:0.4 #343434, stop:0.2 #343434, stop:0.1 #ffaa00);\n"
-"}\n"
-"\n"
-"QRadioButton::indicator:checked, QRadioButton::indicator:unchecked{\n"
-"    color: #b1b1b1;\n"
-"    background-color: #323232;\n"
-"    border: 1px solid #b1b1b1;\n"
-"    border-radius: 6px;\n"
-"}\n"
-"\n"
-"QRadioButton::indicator:checked\n"
-"{\n"
-"    background-color: qradialgradient(\n"
-"        cx: 0.5, cy: 0.5,\n"
-"        fx: 0.5, fy: 0.5,\n"
-"        radius: 1.0,\n"
-"        stop: 0.25 #ffaa00,\n"
-"        stop: 0.3 #323232\n"
-"    );\n"
-"}\n"
-"\n"
-"QCheckBox::indicator{\n"
-"    color: #b1b1b1;\n"
-"    background-color: #323232;\n"
-"    border: 1px solid #b1b1b1;\n"
-"    width: 9px;\n"
-"    height: 9px;\n"
-"}\n"
-"\n"
-"QRadioButton::indicator\n"
-"{\n"
-"    border-radius: 6px;\n"
-"}\n"
-"\n"
-"QRadioButton::indicator:hover, QCheckBox::indicator:hover\n"
-"{\n"
-"    border: 1px solid #ffaa00;\n"
-"}\n"
-"\n"
-"QCheckBox::indicator:checked\n"
-"{\n"
-"    image:url(:/images/checkbox.png);\n"
-"}\n"
-"\n"
-"QCheckBox::indicator:disabled, QRadioButton::indicator:disabled\n"
-"{\n"
-"    border: 1px solid #444;\n"
-"}")
+        MainWindow.resize(757, 503)
+        MainWindow.setStyleSheet("")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(20, 10, 731, 431))
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setObjectName("label")
         self.horizontalLayout.addWidget(self.label)
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
-        self.pushButton_2 = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName("pushButton_2")
         self.horizontalLayout.addWidget(self.pushButton_2)
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.horizontalLayout.addWidget(self.pushButton_4)
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout.addWidget(self.pushButton_3)
         spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem1)
-        self.pushButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setEnabled(False)
         self.pushButton.setObjectName("pushButton")
         self.horizontalLayout.addWidget(self.pushButton)
         self.verticalLayout.addLayout(self.horizontalLayout)
-        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.horizontalLayoutWidget)
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
         self.plainTextEdit.setEnabled(True)
         font = QtGui.QFont()
         font.setPointSize(12)
@@ -509,16 +55,33 @@ class Ui_MainWindow(object):
         self.plainTextEdit.setReadOnly(True)
         self.plainTextEdit.setObjectName("plainTextEdit")
         self.verticalLayout.addWidget(self.plainTextEdit)
+        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.verticalLayout.addItem(spacerItem2)
+        self.tableWidget_2 = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget_2.setEnabled(False)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.tableWidget_2.sizePolicy().hasHeightForWidth())
+        self.tableWidget_2.setSizePolicy(sizePolicy)
+        self.tableWidget_2.setObjectName("tableWidget_2")
+        self.tableWidget_2.setColumnCount(2)
+        self.tableWidget_2.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget_2.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget_2.setHorizontalHeaderItem(1, item)
+        self.verticalLayout.addWidget(self.tableWidget_2)
         self.horizontalLayout_2.addLayout(self.verticalLayout)
-        self.tableWidget = QtWidgets.QTableWidget(self.horizontalLayoutWidget)
-        self.tableWidget.setEnabled(True)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setEnabled(False)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.tableWidget.sizePolicy().hasHeightForWidth())
         self.tableWidget.setSizePolicy(sizePolicy)
         self.tableWidget.setMinimumSize(QtCore.QSize(0, 0))
-        self.tableWidget.setMaximumSize(QtCore.QSize(202, 16777215))
+        self.tableWidget.setMaximumSize(QtCore.QSize(16777215, 16777215))
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(2)
         self.tableWidget.setRowCount(0)
@@ -527,9 +90,10 @@ class Ui_MainWindow(object):
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(1, item)
         self.horizontalLayout_2.addWidget(self.tableWidget)
+        self.gridLayout.addLayout(self.horizontalLayout_2, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 758, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 757, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -544,7 +108,13 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "Cargar Archivo:"))
         self.pushButton_2.setText(_translate("MainWindow", "Cargar Archivo"))
+        self.pushButton_4.setText(_translate("MainWindow", "Palabras Reservadas"))
+        self.pushButton_3.setText(_translate("MainWindow", "Cargar Automata"))
         self.pushButton.setText(_translate("MainWindow", "Iniciar"))
+        item = self.tableWidget_2.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "Linea"))
+        item = self.tableWidget_2.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Error"))
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("MainWindow", "Token"))
         item = self.tableWidget.horizontalHeaderItem(1)
