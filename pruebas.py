d01fb36269aae40946e5d419537efe4223286974
Jhp32 <marine.dark@gmail.com>
2020-10-22 00:25:43 -0500

    Se termino el analisis lexico-Arreglar bugs

--- pruebas.py
+++ pruebas.py
@@ -1,14 +1,4 @@
-import re
-
-str1="a"
-str2='\d'
-aux=bool(re.search(r'\d', str1))
-if(aux==True):
-    print(aux)
-else:
-    print("False")
-    
-
-linea = "6=>=:10"
-linea = linea.split("=>")
-print(linea)
+pos=0
+pos=type(int(pos))
+pos=pos+1
+print(pos)
