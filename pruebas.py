c3a56c5c01007547ad1fc9df4b41ea2eb4c04d50
Jhp32 <marine.dark@gmail.com>
2020-10-22 21:11:07 -0500

    Respaldo de cambios

--- pruebas.py
+++ pruebas.py
@@ -1,4 +1,14 @@
-pos=0
-pos=type(int(pos))
-pos=pos+1
-print(pos)
+room_num = {'John': 425, 'Liz': 212}
+room_num['Isaac'] = {"joel":19,"patrocop":25}
+room_num['Hilario'] = "Sanchez"
+if(room_num["Hilario"]!="-"):
+    print("Si exite")
+else:
+    print("No existe")
+print("********************")
+
+aux1='a'
+if(aux1.isdigit()):
+    print("digito")
+else:
+    print("letra")
