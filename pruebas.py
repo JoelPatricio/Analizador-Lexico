191b91c99c7644c2d80a02a7eec7eb42ff9583a9
Jhp32 <marine.dark@gmail.com>
2020-10-22 21:29:47 -0500

    Respaldo de cambios

--- pruebas.py
+++ pruebas.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-room_num = {'John': 425, 'Liz': 212}
-room_num['Isaac'] = {"joel":19,"patrocop":25}
-room_num['Hilario'] = "Sanchez"
-if(room_num["Hilario"]!="-"):
-    print("Si exite")
-else:
-    print("No existe")
-print("********************")
-
-aux1='a'
-if(aux1.isdigit()):
-    print("digito")
-else:
-    print("letra")
-=======
 import re
 
 str1="a"
@@ -28,4 +12,3 @@ else:
 linea = "6=>=:10"
 linea = linea.split("=>")
 print(linea)
->>>>>>> a88ddff4170db0cef6f2220b56a757742f843d51
